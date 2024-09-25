9d62b9e326a183e077a47486e9ab5226b4648ac1
Pranav Dutthan K S <k.s.pranav.2004@gmail.com>
2024-09-25 23:08:24 +0530

    configured version inconsistencies & updated contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,21 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("@nomicfoundation/hardhat-ethers");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require("@nomicfoundation/hardhat-chai-matchers");
 
 module.exports = {
   solidity: "0.8.27",
 };
+
+module.exports = {
+  solidity: "0.8.27",
+  paths: {
+    sources: "./contracts",  // Update this with the correct path to your contracts
+    tests: "./test",  // Path to your tests (if they're in the default directory, no need to change)
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
+
